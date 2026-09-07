7648942ab031651809ae4bc82b61157fb97bb3c4
Nipuna <nipunareddy25@gmail.com>
2026-09-07 16:47:45 +0530

    Add pattern problem P4

---
+++ Patterns/P4.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int m,n;
+    cout<<"Enter no of rows and columns:";
+    cin>>m;
+    cin>>n;
+    for(int i=0;i<m;i++){
+        for(int j=0;j<=i;j++){
+            cout<<i+1;
+        }
+        cout<<"\n";
+    }
+}
