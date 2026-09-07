a7fc8506b6c04a4286165149adaeb2a7d858bf52
Nipuna <nipunareddy25@gmail.com>
2026-09-07 19:47:13 +0530

    Add pattern problem P15.cpp

--- Patterns/P14.cpp
+++ Patterns/P14.cpp
@@ -5,8 +5,8 @@ int main(){
     cout<<"enter number:";
     cin>>n;
     for(int i=0;i<n;i++){
-        for(int j=0;j<i+1;j++){
-            char a='A'+i;
+        for(int j=0;j<n-i;j++){
+            char a='A'+j;
             cout<<a;
         }
         cout<<"\n";
