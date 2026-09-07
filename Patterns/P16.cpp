c36c3564aa4851b743d0d3ef884c11aa0c111a7c
Nipuna <nipunareddy25@gmail.com>
2026-09-07 19:58:35 +0530

    Add pattern problem P16

--- Patterns/P16.cpp
+++ Patterns/P16.cpp
@@ -6,9 +6,13 @@ int main(){
     cin>>n;
     for(int i=0;i<n;i++){
         for(int j=0;j<i+1;j++){
-            char a='A'+i;
-            cout<<a;
+            if((i+j)%2==0){
+                cout<<"1";
+            }
+            else{
+                cout<<"0";
+            }
         }
         cout<<"\n";
     }
-}    
+}
--- Patterns/tempCodeRunnerFile.cpp
+++ Patterns/tempCodeRunnerFile.cpp
@@ -6,9 +6,9 @@ int main(){
     cin>>n;
     for(int i=0;i<n;i++){
         for(int j=0;j<i+1;j++){
-            char a='A'+i;
+            char a='A'+j;
             cout<<a;
         }
         cout<<"\n";
     }
-}    
+}
